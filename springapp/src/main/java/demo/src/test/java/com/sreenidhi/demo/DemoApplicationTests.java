9f94e8e86c553e2e0e18694d1ab3095ae5be8a18
Iamneo <techadmin@iamneo.ai>
2023-08-17 06:07:23 +0000

    Backup Commit

---
+++ springapp/src/main/java/demo/src/test/java/com/sreenidhi/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sreenidhi.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
